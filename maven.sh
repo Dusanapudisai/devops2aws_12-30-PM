a7c80edfe9175a2dadbfcf987a6dfa3513821f4f
Dusanapudisai <152851588+Dusanapudisai@users.noreply.github.com>
2024-10-24 12:42:36 +0530

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
 # Installing java
-yum install java-1.8.0-openjdk-devel.x86_64 -y
+#yum install java-1.8.0-openjdk-devel.x86_64 -y
+
+amazon-linux-extras install java-openjdk11 -y
 
 # Install Git
 yum install git -y
